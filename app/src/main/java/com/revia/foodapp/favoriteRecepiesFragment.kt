e27256098c1e51cf94278cfaa3247981f7c81f41
bl-niketb <niket.bhaskar@bridgelabz.com>
2023-03-31 12:09:52 +0530

    Developed: Bottom Bar, Top Bar, Navigation Graph

---
+++ app/src/main/java/com/revia/foodapp/favoriteRecepiesFragment.kt
@@ -0,0 +1,19 @@
+package com.revia.foodapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class favoriteRecepiesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_recepies, container, false)
+    }
+
+}
